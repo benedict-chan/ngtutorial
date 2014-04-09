e3faef760a2540e0247f0dce3f3cbf468b8b535e
Benedict Chan <purple.enchanted@gmail.com>
2014-04-09 17:35:03 +1000

    start tutorial http://www.ng-newsletter.com/posts/building-2048-in-angularjs.html , just have yo angular now as this generator seems not working well, have to run custom bower install

---
+++ 2048/app/scripts/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular
+  .module('2048App', [
+    'ngCookies'
+  ]);
---
+++ 2048/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('2048App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
