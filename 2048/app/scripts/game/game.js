9e525da3549792dd017a6823d3a67e2a5ccdac35
Benedict Chan <purple.enchanted@gmail.com>
2014-06-23 15:29:27 +1000

    added simple skeleton for "moves available" in game manager

--- 2048/app/scripts/game/game.js
+++ 2048/app/scripts/game/game.js
@@ -1,5 +1,7 @@
-angular.module('Game', [])
-.service('GameManager', function(){
+angular.module('Game', ['Grid'])
+.service('GameManager', function(GridService){
+
+	this.grid = GridService.grid;
   // Create a new game
   this.newGame = function() {};
   // Handle the move action
@@ -7,5 +9,9 @@ angular.module('Game', [])
   // Update the score
   this.updateScore = function(newScore) {};
   // Are there moves left?
-  this.movesAvailable = function() {};
+  this.movesAvailable = function() {
+  	return GridService.anyCellsAvailable() ||
+  		GridService.tileMatchesAvailable();
+  };
+  
 });
--- 2048/test/unit/game/game_spec.js
+++ 2048/test/unit/game/game_spec.js
@@ -5,9 +5,50 @@ describe('Game module', function(){
 
 		// Our tests will go below here
 		var gameManager;// instance of the GameManager
+		//Mocking the GridService
+		var _gridService;
+
+		beforeEach(module(function($provide){
+
+			_gridService = {
+				anyCellsAvailable : angular.noop,
+				tileMatchesAvailable: angular.noop
+			};
+
+			$provide.value('GridService', _gridService);
+
+		}));
+
 		beforeEach(inject(function(GameManager){
 			gameManager = GameManager;
 			
 		}));
+
+	    it('should have a GameManager', function() {
+	      expect(gameManager).toBeDefined();
+	    });
+
+		describe('.movesAvailable', function() {
+
+			it('should report true if there are cells available', function() {
+				spyOn(_gridService, 'anyCellsAvailable').andReturn(true);
+				expect(gameManager.movesAvailable()).toBeTruthy();
+			});
+
+			it('should report true if there are matches available', function() {
+				spyOn(_gridService, 'anyCellsAvailable').andReturn(false);
+				spyOn(_gridService, 'tileMatchesAvailable').andReturn(true);
+				expect(gameManager.movesAvailable()).toBeTruthy();
+			});
+
+			it('should report false if there are no cells nor matches available', function() {
+			  spyOn(_gridService, 'anyCellsAvailable').andReturn(false);
+			  spyOn(_gridService, 'tileMatchesAvailable').andReturn(false);
+			  expect(gameManager.movesAvailable()).toBeFalsy();
+			});
+
+		});
+
+
 	});
 });
